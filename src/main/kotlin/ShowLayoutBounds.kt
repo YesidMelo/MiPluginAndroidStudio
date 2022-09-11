87bd4f60454ae2ebdc36dd5fea688757334a9f91
YesidMelo <trigoperfec3@hotmail.com>
2022-09-11 17:10:34 -0500

    Commit inicial

---
+++ src/main/kotlin/ShowLayoutBounds.kt
@@ -0,0 +1,22 @@
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.Project
+
+class ShowLayoutBounds : AnAction() {
+    override fun actionPerformed(event : AnActionEvent) {
+        event.project?.showNotification("hola mundo")
+    }
+
+    private fun Project.showNotification(message : String) {
+        NotificationGroup("someId", NotificationDisplayType.BALLOON)
+            .createNotification(
+                "title",
+                message,
+                NotificationType.WARNING,
+                null
+            ).notify(this)
+    }
+}
